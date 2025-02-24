6281c6c06890ebaaf8657bc111480977fb7923cb
Darius <darius.goia@stud.ubbcluj.ro>
2025-02-24 11:12:01 +0200

    Update 1

---
+++ modul/src/Printer.java
@@ -0,0 +1,7 @@
+class Printer {
+    public void printMessages() {
+        System.out.println("Salut! Sper că ai o zi bună!");
+        System.out.println("Astazi este luni");
+        System.out.println("Succes!");
+    }
+}
